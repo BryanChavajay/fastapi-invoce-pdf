bbf78ed7506f36c9b440d95ddf0c5378db229985
Bryan Chavajay <bchavajaydev@gmail.com>
2024-05-18 16:42:30 -0600

    Primera version

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+# Partimos de la imagen de UBUNTU 22.04
+FROM ubuntu:22.04
+
+# Actualizamos los repositorios e instalamos Python
+# RUN sed 's/main$/main universe/' -i /etc/apt/sources.list
+RUN apt-get update
+RUN apt-get upgrade -y
+
+# Download and install wkhtmltopdf
+RUN apt-get install -y build-essential xorg libssl-dev libxrender-dev wget gdebi python3 python3-pip
+
+RUN wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+RUN gdebi --n wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+# ENTRYPOINT ["wkhtmltopdf"]
+
+# Show the extended help
+# CMD ["-h"]
+
+# Ejecuta "which wkhtmltopdf" y guarda la ruta en una variable de entorno
+RUN WKHTMLTOPDF_PATH=$(which wkhtmltopdf) && \
+    echo "export WKHTMLTOPDF_PATH=$WKHTMLTOPDF_PATH" >> /etc/profile
+
+# Activa la variable de entorno en el perfil
+RUN echo "source /etc/profile" >> ~/.bashrc
+
+# Reinicia el shell
+RUN exec bash
+
+# Creamos un directorio de trabajo
+WORKDIR /code
+
+# Copiamos requirements en la carpeta de trabjo
+COPY ./requirements.txt /code/requirements.txt
+
+# Instalamos las dependencias de python
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+# Copiamos el codigo en el directorio
+COPY . ./code
+
+# Exponemos un puerto para currer la aplicacion
+EXPOSE 8080
+
+# Levantamos el servidor
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
---
+++ main.py
@@ -0,0 +1,80 @@
+import pdfkit
+from jinja2 import Environment, FileSystemLoader
+import os
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from decouple import config as con
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/invoce")
+def send_invoce():
+    env = Environment(loader=FileSystemLoader("./"))
+    template = env.get_template("template-factura.html")
+
+    detalle_compra = [
+        {
+            "cantidad": 2,
+            "producto": """ASUS FA506IHR-HN08 RYZEN 5 3GHZ 8GB 512GB SSD 15.6 W11H GTX1650
+                4GB Garantia: 12 Mes(es) S/N: R3NRCX03B007116, R3NRCX03A98611A""",
+            "precio_unitario": 6817.00,
+            "descuento": 954.00,
+            "sub_total": 12680,
+        },
+        {
+            "cantidad": 2,
+            "producto": """ASUS FA506IHR-HN08 RYZEN 5 3GHZ 8GB 512GB SSD 15.6 W11H GTX1650
+                4GB Garantia: 12 Mes(es) S/N: R3NRCX03B007116, R3NRCX03A98611A""",
+            "precio_unitario": 6817.00,
+            "descuento": 954.00,
+            "sub_total": 12680,
+        },
+    ]
+
+    template_vars = {
+        "nit_comprador": "12345678-9",
+        "nombre_comprador": "DOE,JOHN",
+        "imagen_url": "'file:///" + os.getcwd().replace("\\", "/") + "/logo-empresa.jpg'",
+        "detalle_compra": detalle_compra,
+    }
+
+    html_string = template.render(template_vars)
+
+    path_wkhtmltopdf = con('WKHTMLTOPDF_PATH')
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
+    ## Newly added options
+    options = {
+        "page-size": "Letter",
+        "margin-top": "0.5in",
+        "margin-right": "0.5in",
+        "margin-bottom": "0.5in",
+        "margin-left": "0.5in",
+        "encoding": "UTF-8",
+        "enable-local-file-access": None,
+        "no-outline": None,
+        "orientation": "Portrait",
+        # "header-font-name": "Arial",
+        # "header-font-size": 9,
+        # "header-center": "Probando hacer facturas",
+        "footer-font-name": "Arial",
+        "footer-font-size": 9,
+        # "footer-right": "Created on " + str(my_date),
+        "footer-center": "Página [page] de [topage]",
+        # "footer-left": "Created by: JohnDoe",
+        "enable-toc-back-links": None,
+        "images": True,
+    }
+
+    pdfkit.from_string(
+        html_string, "factura.pdf", configuration=config, options=options, css="estilos.css"
+    )
+
+    invoce_path = os.getcwd().replace("\\", "/") + "/factura.pdf"
+
+    return FileResponse(invoce_path)
+
